113a4fa3d70ae5225bb40f2e07a3f98b9ffae9c2
Saveliy Dmitriyev <saveliy@Saveliys-MacBook-Air.local>
2026-05-13 20:17:23 +0500

    Add comments in mcp weather agent

--- ai-weather-agent/src/loaders/mcp.ts
+++ ai-weather-agent/src/loaders/mcp.ts
@@ -5,11 +5,13 @@ import { registerMcpTools } from '../tools/tools';
 
 const transports = new Map<string, SSEServerTransport>();
 
+// Creating MCP Server
 const server = new McpServer({
   name: 'weather-service',
   version: '1.0.0',
 });
 
+// Registering MCP Tools
 registerMcpTools(server);
 
 export { server, transports };
